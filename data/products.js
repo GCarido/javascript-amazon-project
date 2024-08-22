fda8179dc9ce713971ca27165cd7a1f584d5b498
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-22 12:02:08 +0800

    Load product test before all hook jasmine

--- data/products.js
+++ data/products.js
@@ -109,6 +109,8 @@ export function loadProducts(fun) {
 
 
 
+
+
 // export const products = [
 //   {
 //     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,10 +1,17 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadFromStorage } from "../../data/cart.js";
+import { loadProducts } from "../../data/products.js";
 
 describe('test suite: renderOrderSummary', () => {
     const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6';
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d';
     
+    beforeAll((done) => {
+        loadProducts(() => {
+            done();
+        });
+    });
+
     beforeEach(() => {
         document.querySelector('.js-test-container').innerHTML = `
         <div class="js-order-summary"></div>
@@ -45,11 +52,11 @@ describe('test suite: renderOrderSummary', () => {
 
     it('removes a product', () => {
 
-        const deleteLink = document.querySelector(`.js-delete-link-${productId1}`);
+        const deleteLink = document.querySelector(`.js-delete-link-test-suite-${productId1}`);
         if (deleteLink) {
             deleteLink.click();
         } else {
-            console.error(`Element with selector .js-delete-link-${productId1} not found.`);
+            console.error(`Element with selector .js-delete-link-test-suite-${productId1} not found.`);
         }
 
         expect(
