82351203b4a1fc2a76b104b0bcf061c388c59537
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-22 16:27:35 +0800

    Convert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,56 @@
-import { renderOrderSummary} from './checkout/orderSummary.js';
+import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
+import { loadProducts } from '../data/products.js';
 // import '../data/cart-oop.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
-import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 
-loadProducts(() => {
+
+//Use promise.all to run promises at the same time
+
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1'); //parameters inside resolve will be saved in next .then parameter
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+
+]).then((values) => {
+    console.log(values); //TEST to promises: (2) ['value1', undefined]
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+/*
+new Promise((resolve) => {
+
+    loadProducts(() => {
+        resolve('value1'); //parameters inside resolve will be saved in next .then parameter
+    });
+}).then((value) => {
+    console.log(value); //to test it
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    });
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+});
+*/
+
+
+// loadProducts(() => { //USE PROMISE INSTEAD OF CALLBACKS: PROBLEM: NESTING
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     });
+// });
