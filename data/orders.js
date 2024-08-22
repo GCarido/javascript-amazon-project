7693e7325b6d35fb9be5d334345f3afb9c687b1b
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-08-22 21:13:44 +0800

    Create order using backend

---
+++ data/orders.js
@@ -0,0 +1,11 @@
+const getItem = JSON.parse(localStorage.getItem('orders'));
+export const orders = getItem || [];
+
+export function addOrder(order) {
+    orders.unshift(order);
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('orders', JSON.stringify(orders));
+}
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -2,35 +2,36 @@ import { cart } from '../../data/cart.js';
 import { getProduct } from '../../data/products.js';
 import { getDelivery } from '../../data/deliveryOptions.js';
 import formatCurrency from '../utils/money.js';
+import { addOrder } from '../../data/orders.js';
 
-export function renderPaymentSummary(){
+export function renderPaymentSummary() {
 
-    let productPriceCents = 0;
-    let shippingPriceCents = 0;
-    cart.forEach((cartItem, index) => {
-        cartItem = cart[index];
+  let productPriceCents = 0;
+  let shippingPriceCents = 0;
+  cart.forEach((cartItem, index) => {
+    cartItem = cart[index];
 
-        const { productId, quantity, deliveryOptionId } = cartItem;
-        const matchingProduct = getProduct(productId);
-        const { priceCents } = matchingProduct;
-        productPriceCents += priceCents * quantity;
+    const { productId, quantity, deliveryOptionId } = cartItem;
+    const matchingProduct = getProduct(productId);
+    const { priceCents } = matchingProduct;
+    productPriceCents += priceCents * quantity;
 
-        const deliveryOption = getDelivery(deliveryOptionId);
-        shippingPriceCents += deliveryOption.priceCents;
-    });
+    const deliveryOption = getDelivery(deliveryOptionId);
+    shippingPriceCents += deliveryOption.priceCents;
+  });
 
-    let cartQuantity = 0;
+  let cartQuantity = 0;
 
-    cart.forEach((cartItem) => {
-        cartQuantity += cartItem.quantity;
-    });
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
 
-    const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
-    const estimatedTax = totalBeforeTaxCents * 0.1;
+  const totalBeforeTaxCents = productPriceCents + shippingPriceCents;
+  const estimatedTax = totalBeforeTaxCents * 0.1;
 
-    const totalCents = totalBeforeTaxCents + estimatedTax;
+  const totalCents = totalBeforeTaxCents + estimatedTax;
 
-    const paymentSummaryHTML = `
+  const paymentSummaryHTML = `
           <div class="payment-summary-title">
             Order Summary
           </div>
@@ -60,11 +61,30 @@ export function renderPaymentSummary(){
             <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
     `;
 
-    document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
-    
+  document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order').addEventListener('click', async () => {
+    try {
+      const response = await fetch('https://supersimplebackend.dev/orders', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          cart: cart
+        })
+      });
+      const order = await response.json();
+      addOrder(order);
+    } catch (error) {
+      console.log('Unexpect Error. Try again later');
+    }
+
+    window.location.href = 'orders.html';
+  });
 }
